0156cdf6abc7332e5fbacd933fa16ac0d48d47e5
cccc808 <303785860@qq.com>
2025-08-19 00:17:43 +0800

    Add files via upload

--- findsingleitem.go
+++ findsingleitem.go
@@ -2,25 +2,25 @@ package main
 
 import "fmt"
 
-func findsingleitem(arr []int) []int{
+func findsingleitem(arr []int) []int {
 	mapp := make(map[int]int)
-	for _, val := range arr{
+	for _, val := range arr {
 		mapp[val]++
 	}
-	fmt.Println("mapp=",mapp)
+	fmt.Println("mapp=", mapp)
 	var singleitem []int
-	for key,count := range mapp{
-		if count == 1{
+	for key, count := range mapp {
+		if count == 1 {
 			singleitem = append(singleitem, key)
 		}
 	}
 
 	return singleitem
 }
-func main(){
-	arr := [] int{1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,9,9,10,10}
+func main() {
+	arr := []int{1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 9, 9, 10, 10}
 
 	singleitem := findsingleitem(arr)
-	fmt.Println(singleitem)
+	fmt.Println("singleitem: ", singleitem)
 
 }
